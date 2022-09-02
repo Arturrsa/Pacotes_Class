ccc1b82bec68f7c025f00f46a4cc6fa92a3ce3fc
artur henrique <arturr.sanntos@gmail.com>
2022-09-02 10:31:17 -0300

    Create Ex.java

---
+++ Exercícios SOFTEX/Ex.java
@@ -0,0 +1,34 @@
+package br.com.petrobras.administracao;
+
+
+public class TransacaoBancaria {
+    public static void main(String[] args)  {
+        int contador = 11;
+        while (cntador < 0)  {
+            system.out.println(++contador);
+        }
+        contador = 0;
+        do {
+             system.out.println(+contador++);
+        } while (contador < 10);
+
+        for (int i = 0; i < 10; i++) {
+             System.out.println(+ i);
+             if(i==5){
+                 break;
+             }
+         }
+    }
+}   
+
+
+
+package br.com.petrobras.logistica; 
+   
+public class TransacaoBancaria {
+    public static void main(String[] args)  {
+        int idade = 10;
+        float salario = 5000f;
+            system.out.println(idade >=16 & salario >= 2000);
+     }
+}
